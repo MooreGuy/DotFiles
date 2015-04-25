b1848321bfbb44902a6f0928e16022f35d6b87b0
Guy Moore <guy.moore.normal@gmail.com>
2015-04-24 23:39:33 -0700

    Add tmux config install script.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+scriptPath=$(pwd)
 curVimRCPath="./.vimrc"
 oldPathExtension=".old"
 vimRCPath="$HOME/.vimrc"
 vimBundlePath="$HOME/.vim/bundle"
 vimAutoloadPath="$HOME/.vim/autoload"
 pathogenPath="$HOME/.vim/autoload/pathogen.vim"
+tmuxConfigPath="$HOME/tmux.conf"
+tmuxCurrentPath="./tmux.conf"
+vimColors="./vim-colors-solarized"
 
 if [ -a $vimRcPath ];
   then
@@ -30,12 +34,28 @@ then
 	mkdir -v -p $vimAutoloadPath
 fi
 
-
 cd  ~/.vim/bundle
-git clone https://github.com/altercation/vim-colors-solarized
+
+if [ ! -d $vimColors ];
+then
+	git clone https://github.com/altercation/vim-colors-solarized
+else
+	echo "vim-colors-solarized already exists."
+fi
 
 if [ ! -a $pathogenPath ]
 then
 	echo "Getting pathogen script."
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+cd $scriptPath
+if [ -a $tmuxConfigPath ];
+then
+	echo "Tmux config already exists."
+	cp -v "$tmuxConfigPath" "$tmuxConfigPath$oldPathExtension"
+	cp -v "$tmuxCurrentPath" "$tmuxConfigPath"
+else
+	echo "Tmux config doesn't already exist."
+	cp -v "$tmuxCurrentPath" "$tmuxConfigPath"
 fi
